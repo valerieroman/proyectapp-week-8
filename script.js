6ad1bf6bb0ad574a927ac94229a03ff028610a63
valerieroman <81587687+valerieroman@users.noreply.github.com>
2021-05-22 16:05:38 -0400

    Fixed api url

--- script.js
+++ script.js
@@ -65,8 +65,10 @@
     document.querySelector("#overall-weather").innerHTML =
       response.data.weather[0].main;
   
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2px.png`);
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+    iconElement.setAttribute("src", 
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", 
+      response.data.weather[0].description);
   }
 
   function searchCity(city) {
